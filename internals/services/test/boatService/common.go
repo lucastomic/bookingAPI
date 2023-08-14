930a699d0669b3feca9368cc768efffb407283b7
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-08-14 23:24:50 +0200

    Mockup for testing

--- internals/services/test/boatService/common.go
+++ internals/services/test/boatService/common.go
@@ -1,10 +1,19 @@
 package boatservicetests
 
 import (
+	databaseport "github.com/lucastomic/naturalYSalvajeRent/internals/database/ports"
 	"github.com/lucastomic/naturalYSalvajeRent/internals/domain"
-	serviceinjector "github.com/lucastomic/naturalYSalvajeRent/internals/services/injection"
+	"github.com/lucastomic/naturalYSalvajeRent/internals/services"
+	serviceports "github.com/lucastomic/naturalYSalvajeRent/internals/services/ports"
+	"github.com/lucastomic/naturalYSalvajeRent/internals/testing/mockups"
 )
 
 // TODO MUST BE REPLACED BY A BOATSERVICE WITH A MOCKED REPOSITORY
-var boatService = serviceinjector.NewBoatService()
+
+var boatService = func() serviceports.IBoatService {
+	repo := mockups.BoatMockUp{}
+	reservationRepo := databaseport.NewReservationRepository()
+	return services.NewBoatService(repo, reservationRepo)
+}()
+
 var user1 = domain.NewClient("Lucas Tomic", "1234212")
