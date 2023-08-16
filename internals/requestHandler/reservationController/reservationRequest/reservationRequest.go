3093b3243c72a990da95e76b217fe46a7ae5adb0
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-08-16 12:20:34 +0200

    Reservation request updated to new Reservation

--- internals/requestHandler/reservationController/reservationRequest/reservationRequest.go
+++ internals/requestHandler/reservationController/reservationRequest/reservationRequest.go
@@ -1,9 +1,11 @@
 package reservationrequest
 
 type ReservationRequest struct {
-	Email    string `json:"email"`
-	Phone    string `json:"phone"`
-	FirstDay string `json:"firstDay"`
-	LastDay  string `json:"lastDay"`
-	BoatId   int    `json:"boatId"`
+	Email      string `json:"email"`
+	Phone      string `json:"phone"`
+	FirstDay   string `json:"firstDay"`
+	LastDay    string `json:"lastDay"`
+	BoatId     int    `json:"boatId"`
+	Passengers int    `json:"passengers"`
+	IsOpen     bool   `json:"isOpen"`
 }
