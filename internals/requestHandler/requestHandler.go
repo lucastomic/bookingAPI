767a924e33c8f3dca67ded6ac63cd351107f1b97
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-08-19 15:16:40 +0200

    Authentication removed

--- internals/requestHandler/requestHandler.go
+++ internals/requestHandler/requestHandler.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/lucastomic/naturalYSalvajeRent/internals/middelware"
 	authenticationcontroller "github.com/lucastomic/naturalYSalvajeRent/internals/requestHandler/authenticationController"
 	"github.com/lucastomic/naturalYSalvajeRent/internals/requestHandler/boatController"
 	reservationcontroller "github.com/lucastomic/naturalYSalvajeRent/internals/requestHandler/reservationController"
@@ -20,12 +19,12 @@ var (
 func Run() {
 
 	r := gin.Default()
-	secure := r.Group("/").Use(middelware.Auth())
-	{
-		boatController.AddEndpoints(&secure)
-		stateroomcontroller.AddEndpoints(&secure)
-		reservationcontroller.AddEndpoints(&secure)
-	}
+	// secure := r.Group("/").Use(middelware.Auth())
+	// {
+	boatController.AddEndpoints(&secure)
+	stateroomcontroller.AddEndpoints(&secure)
+	reservationcontroller.AddEndpoints(&secure)
+	// }
 	authenticationcontroller.AddEndpoints(r)
 
 	// r.RunTLS(":8080", serverCert, serverKey)
