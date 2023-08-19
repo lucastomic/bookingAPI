c7e5819cb05ba2fbbeb89164c5f1a0ccdbf89f58
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-08-19 16:49:28 +0200

    Unused methods

--- internals/services/ports/IBoatService.go
+++ internals/services/ports/IBoatService.go
@@ -21,15 +21,9 @@ type IBoatService interface {
 	// GetAllBoats retrieves all the boats.
 	// It returns a slice of domain.Boat objects along with any error encountered.
 	GetAllBoats() ([]*domain.Boat, error)
-	// GetFullCapacityDays returns a list of dates on which the specified boat is fully booked.
-	// It takes a domain.Boat object as input and returns a slice of strings representing the full capacity days.
-	GetFullCapacityDays(domain.Boat) []string
 	// GetNotEmptyDays returns a list of dates on which the specified boat has at least one reservation.
 	// It takes a domain.Boat object as input and returns a slice of strings representing the non-empty days.
 	GetNotEmptyDays(domain.Boat) []string
-
-	GetNotAvailableDaysForSharedReservation(boat domain.Boat, passengers int) []string
-
 	// AddReservation adds a new reservation to the specified boat.
 	// It takes a domain.Boat object and a domain.Reservation object as input and returns an error if any occurred.
 	// For example, it would return an error if the reservation can't be allocated because there is not enough space.
@@ -38,4 +32,5 @@ type IBoatService interface {
 	// It takes a domain.Boat object and a domain.Reservation object as input and returns an error if any occurred.
 	// For example, it would return an error if the reservation can't be allocated because there is not enough space.
 	ResevateFullBoat(boat domain.Boat, reservation domain.Reservation) error
+	GetNotAvailableDaysForSharedReservation(boat domain.Boat, passengers int) []string
 }
