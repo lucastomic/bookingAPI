592859d7cb13ee72dcedba9ab0564db7f6e0605e
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-09-13 17:15:37 +0200

    Works, not clean

--- internals/domain/reservationHandler.go
+++ internals/domain/reservationHandler.go
@@ -4,18 +4,35 @@ import (
 	"errors"
 )
 
-type ReservationHandler struct {
+type ReservationManager struct {
 	staterooms  []*StateRoom
 	maxCapacity int
 }
 
-func (b *ReservationHandler) ReservateStaterooms(reservation *Reservation, staterooms int) error {
+func (b *ReservationManager) HasDisponibilityFor(
+	reservation Reservation,
+	stateroomsNeeded int,
+) bool {
+	availableStaterooms := 0
+	for _, stateroom := range b.staterooms {
+		if stateroom.CanReservate(reservation) {
+			availableStaterooms++
+		}
+	}
+	return availableStaterooms >= stateroomsNeeded
+}
+
+func (b *ReservationManager) HasDisponibilityForEntireBoat(reservation Reservation) bool {
+	return b.HasDisponibilityFor(reservation, len(b.staterooms))
+}
+
+func (b *ReservationManager) ReservateStaterooms(reservation *Reservation, staterooms int) error {
 	if !b.HasDisponibilityFor(*reservation, staterooms) {
 		return errors.New("there is not enough space for this reservation")
 	}
-	if !reservation.isOpen {
+	if reservation.isOpen {
 		return errors.New(
-			"only close reservations can reserve only one stateroom. Shared resrevationss must reservate all of them",
+			"only close reservations can reserve only one stateroom. Shared resrevations must reservate all of them",
 		)
 	}
 	stateroomReserved := 0
@@ -32,11 +49,11 @@ func (b *ReservationHandler) ReservateStaterooms(reservation *Reservation, state
 	return nil
 }
 
-func (b *ReservationHandler) ReservateStateroom(reservation *Reservation) error {
+func (b *ReservationManager) ReservateStateroom(reservation *Reservation) error {
 	return b.ReservateStaterooms(reservation, 1)
 }
 
-func (b *ReservationHandler) ReservateEveryStateroom(reservation *Reservation) error {
+func (b *ReservationManager) ReservateEveryStateroom(reservation *Reservation) error {
 	if !b.HasDisponibilityForEntireBoat(*reservation) {
 		return errors.New("there is not enough space for this reservation")
 	}
@@ -48,21 +65,4 @@ func (b *ReservationHandler) ReservateEveryStateroom(reservation *Reservation) e
 		reservation.SetMaxCapacity(b.maxCapacity)
 	}
 	return nil
-}
-
-func (b *ReservationHandler) HasDisponibilityFor(
-	reservation Reservation,
-	stateroomsNeeded int,
-) bool {
-	availableStaterooms := 0
-	for _, stateroom := range b.staterooms {
-		if stateroom.CanReservate(reservation) {
-			availableStaterooms++
-		}
-	}
-	return availableStaterooms >= stateroomsNeeded
-}
-
-func (b *ReservationHandler) HasDisponibilityForEntireBoat(reservation Reservation) bool {
-	return b.HasDisponibilityFor(reservation, len(b.staterooms))
 }
